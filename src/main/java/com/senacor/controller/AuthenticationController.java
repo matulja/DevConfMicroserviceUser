8a1523f8a3fd1b6a1b2de6c96e63cc3eca65c098
matulja <maryna.fehler@gmail.com>
2016-11-30 16:12:40 +0100

    User changed

--- src/main/java/com/senacor/controller/AuthenticationController.java
+++ src/main/java/com/senacor/controller/AuthenticationController.java
@@ -1,14 +1,14 @@
 package com.senacor.controller;
 
 import com.senacor.model.User;
-import com.senacor.repository.UserRepository;
 import com.senacor.service.UserService;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Marynasuprun on 07.11.2016.
@@ -16,14 +16,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class AuthenticationController {
 
     @Autowired
     UserService userService;
 
     @RequestMapping(value = "/auth", method = RequestMethod.POST,  produces = "application/json", consumes = "application/json")
-    public User authenticateUser(@RequestBody User user) {
-       return user = userService.authenticateUser(user);
+    public ResponseEntity <User> authenticateUser(@RequestBody User user) {
+
+        System.out.println("in Login");
+        User loginUser = userService.authenticateUser(user);
+
+        if(loginUser != null){
+            return new ResponseEntity<>(loginUser, HttpStatus.OK);
+        } else{
+            return new ResponseEntity<>(loginUser, HttpStatus.UNAUTHORIZED);
+        }
+
+
        /* if(user != null) {
             return new ResponseEntity<>(user, HttpStatus.OK);
         } else {
--- src/main/java/com/senacor/repository/UserRepository.java
+++ src/main/java/com/senacor/repository/UserRepository.java
@@ -3,17 +3,14 @@ package com.senacor.repository;
 import com.senacor.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * Created by Marynasuprun on 07.11.2016.
  */
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+//@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends MongoRepository<User, String> {
 
     User findByUsername(@Param("username") String username);
